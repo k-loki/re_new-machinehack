05a59ffbd692a7606b9de3531c3e044b57ff6252
Loki <kurmapu000@gmail.com>
2022-08-27 13:20:57 +0530

    cb_5k - 0.01785

--- src/engine.py
+++ src/engine.py
@@ -1,8 +1,6 @@
 # ONLY models go here...
-from math import remainder
 import sys
 sys.path.append(r'D:\Hacks\re_new')
-
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -12,6 +10,7 @@ from category_encoders.target_encoder import TargetEncoder
 from catboost import CatBoostRegressor
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
+from sklearn.ensemble import RandomForestRegressor
 
 # load config
 config = load_config()
@@ -19,7 +18,8 @@ config = load_config()
 
 def get_model(**kwargs):
 
-    model = CatBoostRegressor(n_estimators=2000, verbose=1000, task_type='GPU')
+    model = CatBoostRegressor(n_estimators=5000, verbose=1000, task_type='GPU', random_seed=config['RAND'])
+    # model = RandomForestRegressor(n_estimators=200, verbose=True, random_state=config['RAND'])
     tme = TargetEncoder()
     ct = make_column_transformer(
         (tme, cat_cols),
