5be1796d95cb0e3503c20b3ee942d406f3876a4d
Loki <kurmapu000@gmail.com>
2022-08-27 13:20:57 +0530

    cb_5k - 0.1785

--- src/train_and_eval.py
+++ src/train_and_eval.py
@@ -27,6 +27,7 @@ model_config = config["MODEL"]
 # Load data
 train_data_path = config['train_data']
 train_data = pd.read_csv(train_data_path)
+test_data = pd.read_csv(config['test_data'])
 
 
 def train_and_eval(X_train, y_train, X_val, y_val):
@@ -34,7 +35,7 @@ def train_and_eval(X_train, y_train, X_val, y_val):
     model = get_model()
     model.fit(X_train, y_train)
     train_score = comp_score(model.predict(X_train), y_train)
-    print("Traing mape: ", train_score)
+    print("Training mape: ", train_score)
 
     print('Validating Model..')
     preds = model.predict(X_val)
@@ -48,7 +49,13 @@ def train_and_eval(X_train, y_train, X_val, y_val):
 def __cross_validate(holdout=False, cv_predict=False, wandb_track=True):
     cv_scores = []
 
-    drop_cols = ['timestamp', 'fold']
+    drop_cols = ['timestamp', 'fold', 'Target']
+    
+    if cv_predict:
+        cvpreds_test = np.zeros(shape=(len(test_data), config['N_FOLDS']))
+        cvpreds_train = np.zeros(shape=(len(train_data)))
+
+        print('cv predict...')
 
     for fold in range(config['N_FOLDS']):
         print(f'Fold : {fold}')
@@ -61,7 +68,9 @@ def __cross_validate(holdout=False, cv_predict=False, wandb_track=True):
         X_val, y_val = val_fold.drop(
             drop_cols, axis=1), val_fold.Target
 
-        _, train_score, val_score = train_and_eval(
+        val_idx = val_fold.index
+
+        model, train_score, val_score = train_and_eval(
             X_train, y_train, X_val, y_val)
 
         if wandb_track:
@@ -74,6 +83,11 @@ def __cross_validate(holdout=False, cv_predict=False, wandb_track=True):
 
         cv_scores.append(val_score)
 
+        if cv_predict:
+            # save predictions for ensembling
+            cvpreds_test[:, fold] = model.predict(test_data.drop(['timestamp'], axis=1))
+            cvpreds_train[val_idx] = model.predict(X_val)
+            
         print('----------------------------------------------------------')
 
         # # save_model
@@ -84,6 +98,15 @@ def __cross_validate(holdout=False, cv_predict=False, wandb_track=True):
         if holdout == True:
             break
 
+    if cv_predict:
+        print('Saving cross validated predictions...')
+        test_cv = pd.Series(cvpreds_test.mean(axis=1))
+        train_cv = pd.Series(cvpreds_train)
+        print('Test shape: ', test_cv.shape)
+        print('Train shape: ', train_cv.shape)
+        test_cv.to_csv(f"../submissions/{model_config['model_name']}_test_cv.csv", index=False)
+        train_cv.to_csv(f"../submissions/{model_config['model_name']}_train_cv.csv", index=False)
+
     print("AVG mape :", np.array(cv_scores).mean())
 
 
@@ -91,9 +114,9 @@ def cross_validate(holdout=False, wandb_track=True, cv_predict=False):
     if wandb_track:
         # wandb tracking
         with wandb.init(project=config['project'], name=model_config['model_name'], config=config):
-            __cross_validate(holdout, wandb_track=wandb_track)
+            __cross_validate(holdout, wandb_track=wandb_track, cv_predict=cv_predict)
     else:
-        __cross_validate(holdout, wandb_track=wandb_track)
+        __cross_validate(holdout, wandb_track=wandb_track, cv_predict=cv_predict)
         
 
 if __name__ == '__main__':
