52ef706b02b996d5ffd30e273e2b5ff23f524c63
Loki <kurmapu000@gmail.com>
2022-09-01 02:16:53 +0530

    knn-3-T1 - 0.0048093 :(

--- src/engine.py
+++ src/engine.py
@@ -13,7 +13,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from xgboost import XGBRegressor
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
-from sklearn.linear_model import LinearRegression
 from src.utils import load_config
 
 # load config
@@ -26,9 +25,8 @@ def get_model(**kwargs):
     # model = CatBoostRegressor(random_seed=config['RAND'], n_estimators=5000, verbose=1000)
     # model = LGBMRegressor(random_state=config['RAND'], n_estimators=2000, n_jobs=-1, device='gpu', verbose=0, metric='mape')
     # model = XGBRegressor(tree_method='gpu_hist', random_state=config['RAND'], n_estimators=3000, n_jobs=-1, verbose=False)
-    # model = KNeighborsRegressor(n_neighbors=3, weights='distance', algorithm='auto', p=2, metric='minkowski', n_jobs=-1)
+    model = KNeighborsRegressor(n_neighbors=3, weights='distance', algorithm='auto', p=2, metric='minkowski', n_jobs=-1)
     # model = ExtraTreesRegressor(n_estimators=100, random_state=config['RAND'], verbose=1, n_jobs=-1)
-    model = LinearRegression()
 
     tme = TargetEncoder()
     # pf = PolynomialFeatures(degree=2)
